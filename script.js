86cd79013dd5215acc56c7924a2cab917c0b55c3
vvmaas <vhvermaas@gmail.com>
2022-06-29 11:41:05 -0300

    listagem de quizzes

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 /*JavaScript organizado de acordo com ordem de aparição na página
 /*Sendo disposto com 
-/* 1° - Home page
-/* 2° - Tela do quiz
-/* 3° - Criação de quiz*/
+/* 1° - Variáveis globais
+/* 2° - Home page
+/* 3° - Tela do quiz
+/* 4° - Criação de quiz*/
+
+/*INÍCIO GLOBAIS*/
+
+let DOM_home = document.querySelector(".home").innerHTML
+
+/*FIM GLOBAIS*/
+
+
 
 
 /*INÍCIO HOME*/
 
+function toggleHome() {  // função pra fazer aparecer e sumir a Homepage
+    if (DOM_home===""){ // caso esteja vazia
+        DOM_home = "."//aqui entra todo html da home
+    } else { //caso esteja na tela
+        DOM_home = "" // sai da tela pra abrir espaço
+    }
+}
+
+function goToQuizz(quizz) { // função pra limpar a home e abrir o quiz
+    toggleHome()
+
+}
 
 
 /*FIM HOME*/
@@ -17,14 +38,12 @@
 /*INÍCIO QUIZ*/
 
 
-
 /*FIM QUIZ*/
 
 
 
 
 /*INÍCIO CRIAÇÃO*/
-
 
 
 /*FIM CRIAÇÃO*/
