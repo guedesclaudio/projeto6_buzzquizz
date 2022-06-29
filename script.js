2731893af11802eedd88abf52d68e619daec4073
Lucas Zuquetto <lucasdeozuquetto@hotmail.com>
2022-06-29 14:38:50 -0300

    layout exibição quiz

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
 /*INÍCIO GLOBAIS*/
 
 let DOM_home = document.querySelector(".home").innerHTML
+let quizzesArray = []
 
 /*FIM GLOBAIS*/
 
@@ -17,19 +18,70 @@ let DOM_home = document.querySelector(".home").innerHTML
 /*INÍCIO HOME*/
 
 function toggleHome() {  // função pra fazer aparecer e sumir a Homepage
-    if (DOM_home===""){ // caso esteja vazia
-        DOM_home = "."//aqui entra todo html da home
+    if (DOM_home === ""){ // caso esteja vazia
+        document.querySelector(".home").innerHTML = "."//aqui entra todo html da home //modifiquei aqui porque a função não estava sumindo o conteúdo
     } else { //caso esteja na tela
-        DOM_home = "" // sai da tela pra abrir espaço
+        document.querySelector(".home").innerHTML = "" // sai da tela pra abrir espaço
     }
 }
-
 function goToQuizz(quizz) { // função pra limpar a home e abrir o quiz
+    console.log(quizz)
     toggleHome()
+    document.querySelector('.quiz').innerHTML = `
+        <div class="ImgTopoQuiz">
+            <h2>${quizz.querySelector('span').innerHTML}</h2>
+        </div>
+        <div class="QeAQuiz">
+            <div class="Pergunta">
+                <h3>Pergunta Pergunta</h3>
+            </div>
+            <div class="Resposta">
+                <div class="RespostaColuna1">
+                    <div class="Resposta1">
+                        <img class="imgresposta" src="./hidef.jpg" alt="">
+                        <span>Resposta1</span>
+                    </div>
+                    <div class="Resposta2">
+                        <img class="imgresposta" src="./hidef.jpg" alt="">
+                        <span>Resposta2</span>
+                    </div>
+                </div>
+                <div class="RespostaColuna2">
+                    <div class="Resposta3">
+                        <img class="imgresposta" src="./hidef.jpg" alt="">
+                        <span>Resposta3</span>
+                    </div>
+                    <div class="Resposta4">
+                        <img class="imgresposta" src="./hidef.jpg" alt="">
+                        <span>Resposta4</span>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>`
 
 }
 
+function ObterQuizzes(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
+    promise.then(ObterQuizzesSucesso)
+}
+function ObterQuizzesSucesso(elemento){
+    
+    for(let i = 0; i < elemento.data.length; i++){
+        quizzesArray.push(elemento.data[i])
+        const quizzCardtemplate = `
+        <div class="quizzCard" onclick="goToQuizz(this)">
+            <img src=${elemento.data[i].image}>
+            <span>${elemento.data[i].title}</span>
+        </div>`
+        document.querySelector('.quizzCards').innerHTML += quizzCardtemplate
+    }
+    console.log(elemento)
+    console.log(quizzesArray)
 
+}
+ObterQuizzes()
 /*FIM HOME*/
 
 
