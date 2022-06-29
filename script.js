f72d768f1159c3346a7f8b57a7bd7b0f8bba3260
vvmaas <vhvermaas@gmail.com>
2022-06-29 10:47:32 -0300

    layout base home

---
+++ script.js
@@ -0,0 +1,30 @@
+/*JavaScript organizado de acordo com ordem de aparição na página
+/*Sendo disposto com 
+/* 1° - Home page
+/* 2° - Tela do quiz
+/* 3° - Criação de quiz*/
+
+
+/*INÍCIO HOME*/
+
+
+
+/*FIM HOME*/
+
+
+
+
+/*INÍCIO QUIZ*/
+
+
+
+/*FIM QUIZ*/
+
+
+
+
+/*INÍCIO CRIAÇÃO*/
+
+
+
+/*FIM CRIAÇÃO*/
