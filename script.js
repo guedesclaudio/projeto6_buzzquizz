c6df8f4786242945690a2bfa7d732b240c254ea1
guedesclaudio <cgsarmentosilva@gmail.com>
2022-07-02 14:55:13 -0300

    correções

--- script.js
+++ script.js
@@ -784,6 +784,7 @@ function validaTudoNiveis(icone) {
             let promise = axios.post("https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes", quizObjeto)
             armazenaDadosLocal()
             goToSucessoCriar()
+            //quizzesArray.push(quizObjeto)
         }
        
         
@@ -803,6 +804,7 @@ function validaUrlInicio() {
 function armazenaDadosLocal() {
     let key = quizObjeto.title
     const myQuizz = quizObjeto
+    console.log(quizObjeto)
     const myQuizzSerializado = JSON.stringify(myQuizz)
     localStorage.setItem(key, myQuizzSerializado)
 }
@@ -840,7 +842,7 @@ function apagaQuizzLocal(tituloQuizz) {
 function renderizaUserQuizz(quizzRetornado) {
     const containerUserQuizz = document.querySelector(".quizzCardsUser")
     const templateUserQuizz = `
-    <div class="quizzCard" id="" onclick="goToQuizz(this)">
+    <div class="quizzCard" id="50" onclick="goToQuizz(this)">
         <img src=${quizzRetornado.image}>
         <span>${quizzRetornado.title}</span>
     </div>
