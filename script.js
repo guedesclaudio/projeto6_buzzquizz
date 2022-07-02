07e23364bd031a216ef6d308ab94974a5091d1bc
vvmaas <vhvermaas@gmail.com>
2022-07-02 12:28:43 -0300

    Fix bug Nivel post open

--- script.js
+++ script.js
@@ -783,7 +783,7 @@ function validaTudoNiveis(icone) {
         if (validadorNiveis === DOM_niveis.length - 1) {
             document.querySelector(".botao").innerHTML = `<button class="goToSucesso" onclick="validaTudoNiveis()">Finalizar Quizz</button>`
         } else if (validadorNiveis === DOM_niveis.length) {
-            //let promise = axios.post("https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes", quizObjeto)
+            let promise = axios.post("https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes", quizObjeto)
             armazenaDadosLocal()
             goToSucessoCriar()
         }
